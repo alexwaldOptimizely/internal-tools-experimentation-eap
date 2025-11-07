eea041ab3853a1236f4cbac0239242923a49fabb
Alex Wald <alex.wald@optimizely.com>
2025-11-06 20:50:51 -0600

    Remove name and version from discovery endpoint - Opal may only expect functions

--- api/index.ts
+++ api/index.ts
@@ -63,8 +63,6 @@ app.get('/health', async (req, res) => {
 // Discovery endpoint for Opal
 app.get('/discovery', (req, res) => {
   res.json({
-    name: 'optimizely-jira-integration',
-    version: '1.0.0',
     functions: [
       {
         name: 'create_jira_ticket_DHK',
